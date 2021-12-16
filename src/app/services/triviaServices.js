62c860677817c443abee1c66b1d9d9758a120bcb
Atomics <info@atomicsdigital.com>
2021-12-16 01:16:29 -0500

    fixed icon color bug

--- src/app/services/triviaServices.js
+++ src/app/services/triviaServices.js
@@ -6,8 +6,13 @@ export const getTriviaQuestions = (amount, difficulty, setResults) => {
     url: `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=boolean`,
   }
   return axios.request(options).then((response) => {
-    setResults(response.data.results)
-    console.log(response.data.results)
+    if(response.response_code === 2) {
+      console.log('Please check the api url provided')
+    }
+    else {
+      setResults(response.data.results)
+    }
+    console.log(response.data)
   }).catch((error) => {
     console.error(error)
   })
